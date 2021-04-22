0f22ced9e628380f824ea76b0822fd265e31268f
jb-kerboeuf <jb.kerboeuf@gmail.com>
2021-04-22 16:28:27 +0200

    Refactor chance

--- Yatzy.java
+++ Yatzy.java
@@ -11,14 +11,12 @@ public class Yatzy {
         dice[4] = d5;
     }
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5) {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+    public int chance() {
+        int sum = 0;
+        for (int value : dice) {
+            sum += value;
+        }
+        return sum;
     }
 
     public static int yatzy(int... dice) {
--- YatzyTest.java
+++ YatzyTest.java
@@ -5,8 +5,8 @@ public class YatzyTest {
 
     @Test
     public void chance_scores_sum_of_all_dice() {
-        assertEquals(15, Yatzy.chance(2, 3, 4, 5, 1));
-        assertEquals(16, Yatzy.chance(3, 3, 4, 5, 1));
+        assertEquals(15, new Yatzy(2, 3, 4, 5, 1).chance());
+        assertEquals(16, new Yatzy(3, 3, 4, 5, 1).chance());
     }
 
     @Test
