175bf558aeb5e84f5ad831e6d79748508b0101c9
jb-kerboeuf <jb.kerboeuf@gmail.com>
2021-04-22 15:30:20 +0200

    Inline test values and simplify assert

--- YatzyTest.java
+++ YatzyTest.java
@@ -5,24 +5,20 @@ public class YatzyTest {
 
     @Test
     public void chance_scores_sum_of_all_dice() {
-        int expected = 15;
-        int actual = Yatzy.chance(2, 3, 4, 5, 1);
-        assertEquals(expected, actual);
+        assertEquals(15, Yatzy.chance(2, 3, 4, 5, 1));
         assertEquals(16, Yatzy.chance(3, 3, 4, 5, 1));
     }
 
     @Test
     public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(4, 4, 4, 4, 4);
-        assertEquals(expected, actual);
+        assertEquals(50, Yatzy.yatzy(4, 4, 4, 4, 4));
         assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6));
         assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 3));
     }
 
     @Test
     public void test_1s() {
-        assertTrue(Yatzy.ones(1, 2, 3, 4, 5) == 1);
+        assertEquals(1, Yatzy.ones(1, 2, 3, 4, 5));
         assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
         assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
         assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
