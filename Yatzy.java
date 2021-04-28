5dceeb7c7c525cd95649fd88f10efa94d29474e8
jb-kerboeuf <jb.kerboeuf@gmail.com>
2021-04-28 15:33:36 +0200

    diceFrequency could be a class property

--- Yatzy.java
+++ Yatzy.java
@@ -4,26 +4,43 @@ import java.util.Map;
 
 public class Yatzy {
 
-    protected List<Integer> dice;
+    protected List<Integer> diceValues;
+    protected Map<Integer, Integer> diceFrequency ;
 
     public Yatzy(int d1, int d2, int d3, int d4, int d5) {
-        dice = Arrays.asList(d1, d2, d3, d4, d5);
+        diceValues = Arrays.asList(d1, d2, d3, d4, d5);
+        diceFrequency = new java.util.HashMap<>(Map.of(
+                1, 0,
+                2, 0,
+                3, 0,
+                4, 0,
+                5, 0,
+                6, 0
+        ));
+        updateDiceFrequency();
+    }
+
+    private void updateDiceFrequency() {
+        for (int die : diceValues) {
+            int newCount = diceFrequency.get(die) + 1;
+            diceFrequency.replace(die, newCount);
+        }
     }
 
     public int chance() {
-        return dice.stream().reduce(0,
+        return diceValues.stream().reduce(0,
                 Integer::sum);
     }
 
     public int yatzy() {
-        if (dice.stream().allMatch(
-                dice.get(0)::equals))
+        if (diceValues.stream().allMatch(
+                diceValues.get(0)::equals))
             return 50;
         return 0;
     }
 
     private int simpleNumbers(int num) {
-        return dice.stream().reduce(0,
+        return diceValues.stream().reduce(0,
                 (sum, die) -> die == num ? sum + die : sum);
     }
 
@@ -51,25 +68,9 @@ public class Yatzy {
         return simpleNumbers(6);
     }
 
-    private Map<Integer, Integer> getNumberCounts() {
-        Map<Integer, Integer> numberCounts = new java.util.HashMap<>(Map.of(
-                1, 0,
-                2, 0,
-                3, 0,
-                4, 0,
-                5, 0,
-                6, 0
-        ));
-        for (int die : dice) {
-            int newCount = numberCounts.get(die) + 1;
-            numberCounts.replace(die, newCount);
-        }
-        return numberCounts;
-    }
-
-    private static int nOfAKind(int n, Map<Integer, Integer> numberCounts) {
+    private static int nOfAKind(int n, Map<Integer, Integer> _diceFrequency) {
         for (int dieValue = 6; dieValue > 0; dieValue--) {
-            if (numberCounts.get(dieValue) >= n) {
+            if (_diceFrequency.get(dieValue) >= n) {
                 return dieValue;
             }
         }
@@ -77,16 +78,16 @@ public class Yatzy {
     }
 
     public int pair() {
-        return nOfAKind(2, getNumberCounts()) * 2;
+        return nOfAKind(2, diceFrequency) * 2;
     }
 
     public int twoPairs() {
-        Map<Integer, Integer> numberCounts = getNumberCounts();
-        int highestPair = nOfAKind(2, numberCounts);
+        int highestPair = nOfAKind(2, diceFrequency);
         if (highestPair != 0) {
+            Map<Integer, Integer> newFrequency = new java.util.HashMap<>(diceFrequency);
             // we remove the highestPair dice values so we don't get the same result for secondHighestPair
-            numberCounts.replace(highestPair, 0);
-            int secondHighestPair = nOfAKind(2, numberCounts);
+            newFrequency.replace(highestPair, 0);
+            int secondHighestPair = nOfAKind(2, newFrequency);
             if (secondHighestPair != 0)
                 return highestPair * 2 + secondHighestPair * 2;
         }
@@ -94,42 +95,40 @@ public class Yatzy {
     }
 
     public int threeOfAKind() {
-        return nOfAKind(3, getNumberCounts()) * 3;
+        return nOfAKind(3, diceFrequency) * 3;
     }
 
     public int fourOfAKind() {
-        return nOfAKind(4, getNumberCounts()) * 4;
+        return nOfAKind(4, diceFrequency) * 4;
     }
 
     public int smallStraight() {
-        Map<Integer, Integer> numberCounts = getNumberCounts();
-        if (numberCounts.get(1) == 1 &&
-                numberCounts.get(2) == 1 &&
-                numberCounts.get(3) == 1 &&
-                numberCounts.get(4) == 1 &&
-                numberCounts.get(5) == 1)
+        if (diceFrequency.get(1) == 1 &&
+                diceFrequency.get(2) == 1 &&
+                diceFrequency.get(3) == 1 &&
+                diceFrequency.get(4) == 1 &&
+                diceFrequency.get(5) == 1)
             return 15;
         return 0;
     }
 
     public int largeStraight() {
-        Map<Integer, Integer> numberCounts = getNumberCounts();
-        if (numberCounts.get(2) == 1 &&
-                numberCounts.get(3) == 1 &&
-                numberCounts.get(4) == 1 &&
-                numberCounts.get(5) == 1 &&
-                numberCounts.get(6) == 1)
+        if (diceFrequency.get(2) == 1 &&
+                diceFrequency.get(3) == 1 &&
+                diceFrequency.get(4) == 1 &&
+                diceFrequency.get(5) == 1 &&
+                diceFrequency.get(6) == 1)
             return 20;
         return 0;
     }
 
     public int fullHouse() {
-        Map<Integer, Integer> numberCounts = getNumberCounts();
-        int threeOfAKind = nOfAKind(3, numberCounts);
+        int threeOfAKind = nOfAKind(3, diceFrequency);
         if (threeOfAKind != 0) {
+            Map<Integer, Integer> newFrequency = new java.util.HashMap<>(diceFrequency);
             // we remove the threeOfAKind dice values so we don't get the same result for the pair
-            numberCounts.replace(threeOfAKind, 0);
-            int pair = nOfAKind(2, numberCounts);
+            newFrequency.replace(threeOfAKind, 0);
+            int pair = nOfAKind(2, newFrequency);
             if (pair != 0)
                 return threeOfAKind * 3 + pair * 2;
         }
